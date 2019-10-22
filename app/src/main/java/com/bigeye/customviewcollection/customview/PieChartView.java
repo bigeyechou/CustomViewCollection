89b2b59f30cbae1c0ad5bae84fa98d7e49a1f235
zhengguanzhou <bigeye_chou@163.com>
2019-10-22 11:19:44 +0800

    饼状图

--- app/src/main/java/com/bigeye/customviewcollection/customview/PieChartView.java
+++ app/src/main/java/com/bigeye/customviewcollection/customview/PieChartView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.renderscript.Sampler;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -17,29 +18,50 @@ import java.util.List;
  */
 public class PieChartView extends View {
 
-  private Paint mPaint = new Paint();
+  private Paint paintChart, paintText, paintMasking;
   private RectF mRectF = new RectF();
   private List<PieData> mPieDatas = new ArrayList<>();
   private float mSumValue;
 
   public PieChartView(Context context) {
     super(context);
+    initAll();
   }
 
   public PieChartView(Context context, @Nullable AttributeSet attrs) {
     super(context, attrs);
+    initAll();
   }
 
   public PieChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
+    initAll();
   }
 
-  public void setPieData(List<PieData> pieData){
-    if (pieData.size()>0){
+  private void initAll() {
+    initPain();
+  }
+
+  private void initPain() {
+    paintChart = new Paint();
+
+    paintMasking = new Paint();
+    paintMasking.setColor(Color.WHITE);
+    paintMasking.setStyle(Paint.Style.FILL);
+
+    paintText = new Paint();
+    paintText.setColor(Color.BLACK);
+    paintText.setStyle(Paint.Style.FILL);
+    paintText.setStrokeWidth(8);
+    paintText.setTextSize(20);
+  }
+
+  public void setPieData(List<PieData> pieData) {
+    if (pieData.size() > 0) {
       mPieDatas = pieData;
       if (mPieDatas.size() > 0) {
         int sumValue = 0;
-        for (PieData data : mPieDatas){
+        for (PieData data : mPieDatas) {
           sumValue += data.getValue();
         }
         mSumValue = sumValue;
@@ -47,31 +69,29 @@ public class PieChartView extends View {
     }
   }
 
-
   @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
     super.onMeasure(widthMeasureSpec, heightMeasureSpec);
   }
 
   @Override protected void onSizeChanged(int w, int h, int oldw, int oldh) {
     super.onSizeChanged(w, h, oldw, oldh);
-    mRectF.set(0,0,getWidth(),getHeight());
+    mRectF.set(0, 0, getWidth(), getHeight());
   }
 
   @Override protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
-    if (mPieDatas==null || mPieDatas.size()<=0){
+    if (mPieDatas == null || mPieDatas.size() <= 0) {
       return;
     }
-    float startArc = 0;
-    float endArc=0;
-    for (PieData data : mPieDatas){
-      mPaint.setColor(data.getColor());
-      endArc = (data.getValue()/mSumValue)*360f;
-      mPaint.setColor(data.getColor());
-      canvas.drawArc(mRectF,startArc,endArc,true,mPaint);
-      startArc += endArc;
+    float startAngle = 0;
+    float sweepAngle = 0;
+    for (PieData data : mPieDatas) {
+      paintChart.setColor(data.getColor());
+      sweepAngle = (data.getValue() / mSumValue) * 360f;
+      canvas.drawArc(mRectF, startAngle, sweepAngle - 3f, true, paintChart);
+      startAngle += sweepAngle;
     }
-
+    canvas.drawCircle(getWidth() / 2, getHeight() / 2, getWidth() / 4, paintMasking);
   }
 
 }
